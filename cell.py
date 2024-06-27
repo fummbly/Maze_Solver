6e2722793692923c5d55c79924383411cc508948
fummbly <tstone449@gmail.com>
2024-06-27 14:13:49 -0400

    Refactored Cell to be like bootdotdev

---
+++ cell.py
@@ -0,0 +1,35 @@
+from graphics import Line, Point
+
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
+        self.__y2 = None
+        self.__win = window
+
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        if self.has_left_wall:
+            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(left_wall, "blue")
+
+        if self.has_top_wall:
+            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(top_wall, "red")
+
+        if self.has_right_wall:
+            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(right_wall, "green")
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(bottom_wall, "purple")
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 400)
-    cell1 = Cell(30, 10, 60, 40, win)
-    cell2 = Cell(100, 200, 500, 800, win)
-    cell3 = Cell(70, 80, 100, 110, win)
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell3 = Cell(win)
 
-    cell1.has_right_wall = False
-    cell2.has_top_wall = False
-    cell3.has_left_wall = False
-    cell3.has_bottom_wall = False
 
-    cell1.draw()
-    cell2.draw()
-    cell3.draw()
+    cell1.draw(70, 80, 100, 110)
+    cell2.draw(30, 40, 64, 89)
+    cell3.draw(120, 330, 500, 300)
 
 
     win.wait_for_close()
