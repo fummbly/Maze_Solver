c3da24c66b8a7aae3cd3f39f60a0fe831cd9a61f
fummbly <tstone449@gmail.com>
2024-06-27 13:52:23 -0400

    Added Line and Point classes

--- graphics.py
+++ graphics.py
@@ -1,5 +1,22 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
+
+
+
 class Window :
     def __init__(self, width, height):
         self.__root = Tk()
@@ -19,6 +36,10 @@ class Window :
         self.__running = True
         while self.__running:
             self.redraw()
+
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
-    win = Window(1000, 200)
+    win = Window(800, 400)
+    p1 = Point(0, 0)
+    p2 = Point(100, 200)
+    p3 = Point(10, 15)
+    p4 = Point(12, 25)
+
+    l1 = Line(p1, p2)
+    l2 = Line(p3, p4)
+    l3 = Line(p4, p2)
+
+    win.draw_line(l1, "black")
+    win.draw_line(l2, "red")
+    win.draw_line(l3, "green")
+
     win.wait_for_close()
 
 
